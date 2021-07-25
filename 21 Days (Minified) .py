09eb4080323e9d86315ab571e50e58fdb5aa1493
Arjun Satwani <80617915+arjunsatwani@users.noreply.github.com>
2021-07-25 20:33:10 +0530

    Update 21 Days (Minified) .py

--- 21 Days (Minified) .py
+++ 21 Days (Minified) .py
@@ -6,12 +6,11 @@ main=Tk()
 main.config(bg=_A)
 label=Label(main,text=Num.zfill(2),font=('Helvetica',166,'bold'),fg=_B,bg=_A)
 label.grid(pady=500)
-dr=['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21']
-c=count
+dr=int(Num)
 def Save():
-	global dr,c
-	if c==21:c=0
-	c+=1;data=open('save.py','w');data.write(f"count = {c}\nNum = str({dr[-c]})");data.close();label.config(text=str(dr[-c].zfill(2)));exit()
+	global dr;dr-=1
+	if dr<1:dr=21
+	data=open('save.py','w');data.write(f"Num = str({dr})");data.close();label.config(text=str(dr).zfill(2));exit()
 ask=Button(main,text='CHECK',bd=15,command=Save,bg=_A,fg=_B)
 ask.grid()
 main.mainloop()
