0d9fff97510777829e9c1617e80eae9244932f3d
Arjun Satwani <80617915+arjunsatwani@users.noreply.github.com>
2021-07-27 18:30:09 +0530

    Update save.py

--- save.py
+++ save.py
@@ -1,5 +1,6 @@
 Num = str(21)
-#Add Today's Date In Today variable..
+#Add Today's Date In Today variable & Save It..
+#Now Run The Main Program (21 Days) & DONE..
 Today = "2021-07-27"
 Split = Today.split("-")
 Checked_Days = 0
