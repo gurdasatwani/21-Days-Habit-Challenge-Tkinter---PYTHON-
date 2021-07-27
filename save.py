c9e3b467c11bb0a6de247ce306e0091544f0eca4
Arjun Satwani <80617915+arjunsatwani@users.noreply.github.com>
2021-07-27 18:24:17 +0530

    Update save.py

--- save.py
+++ save.py
@@ -1,3 +1,6 @@
 Num = str(21)
-#Add Today's Date In today variable..
-today = "2021-07-25"
+#Add Today's Date In Today variable..
+Today = "2021-07-27"
+Split = Today.split("-")
+Checked_Days = 0
+Skipped_Days = 0
